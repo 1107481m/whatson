2053c0c7aec46f0eaaf88256d8c986fedfeb4728
1107481m <1107481m@student.gla.ac.uk>
2015-03-05 18:22:19 +0000

    Index page created

--- whatson/urls.py
+++ whatson/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from whatson import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'^home/', views.home, name='home'),
+)
--- whatson/views.py
+++ whatson/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     response = render(request,'index.htm')
     return response
+
+def home(request):
+    response = render(request,'home.htm')
+    return response
